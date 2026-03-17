91655b8769fcf8d33a2c8058c8c130bcc8a08245
Arpana Rani <arpanaaims@gmail.com>
2026-03-17 09:26:46 +0530

    Initial Commit

---
+++ SwiftNote/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftNote
+//
+//  Created by Arpana Rani on 17/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftNote/SwiftNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftNoteApp.swift
+//  SwiftNote
+//
+//  Created by Arpana Rani on 17/03/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
